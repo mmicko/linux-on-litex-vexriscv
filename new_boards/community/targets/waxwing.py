df9d829a81971401d6e14d4d52e1c9dbdd4cf7d1
Miodrag Milanovic <mmicko@gmail.com>
2020-01-09 17:19:56 +0100

    works with latest

--- new_boards/community/targets/waxwing.py
+++ new_boards/community/targets/waxwing.py
@@ -247,14 +247,14 @@ class EthernetSoC(BaseSoC):
         self.add_memory_region("ethmac", self.mem_map["ethmac"], 0x2000, type="io")
         self.add_csr("ethmac")
         self.add_interrupt("ethmac")
-        #self.ethphy.crg.cd_eth_rx.clk.attr.add("keep")
-        #self.ethphy.crg.cd_eth_tx.clk.attr.add("keep")
-        #self.platform.add_period_constraint(self.ethphy.crg.cd_eth_rx.clk, 1e9/12.5e6)
-        #self.platform.add_period_constraint(self.ethphy.crg.cd_eth_tx.clk, 1e9/12.5e6)
-        #self.platform.add_false_path_constraints(
-        #    self.crg.cd_sys.clk,
-        #    self.ethphy.crg.cd_eth_rx.clk,
-        #    self.ethphy.crg.cd_eth_tx.clk)
+        self.ethphy.crg.cd_eth_rx.clk.attr.add("keep")
+        self.ethphy.crg.cd_eth_tx.clk.attr.add("keep")
+        self.platform.add_period_constraint(self.ethphy.crg.cd_eth_rx.clk, 1e9/12.5e6)
+        self.platform.add_period_constraint(self.ethphy.crg.cd_eth_tx.clk, 1e9/12.5e6)
+        self.platform.add_false_path_constraints(
+            self.crg.cd_sys.clk,
+            self.ethphy.crg.cd_eth_rx.clk,
+            self.ethphy.crg.cd_eth_tx.clk)
 
 # Build --------------------------------------------------------------------------------------------
 
